57168f918cccc80aab8279f458beb397b3007343
PNHkkoma <107099281+PNHkkoma@users.noreply.github.com>
2024-09-30 14:27:49 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-https://sci-hub.se/10.1016/j.neucom.2013.05.025
+
 https://d-nb.info/1221936638/34
 https://moscow.sci-hub.se/2326/b9cdc4a37bbb5cdfdaefcd9dd05d6166/wang2013.pdf#page=5&zoom=100,0,0
 
@@ -29,14 +29,6 @@ hung12A52001
 C:\Users\Administrator/.ssh/id_rsa
 (C:\Users\dell/.ssh/id_rsa)
 10.*;192.*;172.*;educationxr.vn;*.viettel.vn
-thùy dung
-sếp Hạnh
-
-phân tích toán học cho thuật toán k-means
-https://pages.github.com/
-https://railway.app/new
-https://docs.netlify.com/
-https://www.uhostfull.com/free-hosting.php
 
 https://www.youtube.com/watch?v=j1IbfQrT2Cs
 
@@ -46,66 +38,14 @@ https://www.youtube.com/@buildamodule một lish chi tiến vl, nhưng cũng có
 https://www.youtube.com/@buildamodule trang web có vẻ ko tín lắm khi các video tương đối ngắn và sơ sài, không có quá nhiều ứng dụng
 
 
-https://d-nb.info/1221936638/34 đây có vẻ chính là cái file giải thích cho hồi quy tuyến tính bị cắt đi của cái video sư phạm việt nam
-https://onlinelibrary.wiley.com/doi/full/10.1111/biom.13465 || A transformation-free linear regression for compositional outcomes and predictors || cái này có vẻ cũng tạm được khi liên quan cả ới hồi quy và dữ liệu da hợp, khá ngắn gọn và ví dụ cũng cụ thể
-https://is.muni.cz/do/rect/habilitace/1431/Hron/habilitace/7_Hron_et_al__2012_.pdf || Linear regression with compositional explanatory variables || có thể tham khảo thêm cái này, mặc dù nó loằng ngoằng khó hiểu hơn
-
-https://dougfenstermacher.com/blog/compositional-data-analysis ||code của Python
-https://cran.r-project.org/web/packages/Compositional/Compositional.pdf || thư viện sử dụng cho R, có vẻ không cần python nè
-https://search.r-project.org/CRAN/refmans/complmrob/html/00Index.html || có vẻ cũng là 1 tài liệu về R
-https://rpubs.com/EmilOWK/compositional_data || 1 ví dụ về R nhưng có vẻ khó vì ko biết bô dữ liệu ở đâu, bối cảnh của nó là gì và ý nghĩa khi chạy xong R là gì.
-https://github.com/muellsen/PCM/tree/master/examples/LogContrastModels || bộ dữ liệu trong cái tài liệu phụ mà ông giáo gửi
-https://zlib.pub/download/geostatistics-for-compositional-data-with-r-use-r-24tneuvforeg?hash=bc956cdb5dedf1f396ceafb3b6c62012 //trang 236 là cái gì đó
-
-https://arxiv.org/pdf/2112.14865.pdf cái này thực sự thì kha khá ngắn và ít thông tin như có vẻ nó sẽ có quy trình hợp với ý tưởng của thầy, nhưng có vẻ cấp số nhân ở đây ko dc hợp lý lắm...
-https://www.york.ac.uk/media/economics/documents/hedg/workingpapers/2023/2316.pdf đây có lẽ là 1 phần ứng dụng của 1 bài nào đó bên trên, ko có nhiều ý nghĩa lý thuyết lắm
-https://www.ncbi.nlm.nih.gov/pmc/articles/PMC9041651/ cái này, có vẻ cũng ko có cái gì mới hơn so với những cái bên trên hmmm...ít nhất thì ví dụ khá dễ nhìn
-http://file.statistik.tuwien.ac.at/filz/papers/2011JApplStats.pdf || Linear regression with compositional explanatory variables || hmmm...bí quá thì đọc, vì có vẻ nó cũng có cùng ý tưởng với mấy cái trên
-https://mpra.ub.uni-muenchen.de/67868/1/MPRA_paper_67868.pdf ||Regression analysis with compositional data containing zero values|| cái này có vẻ cũng trùng ý tưởng nhiều này, hmmm...
-file:///C:/Users/Administrator/Downloads/matthias,+Journal+manager,+mueller_edited.pdf || Interpretation of Compositional Regression with Application to Time Budget Analysis || cái này rất nhiều toán học nhưng lại rất ít khả năng dùng vì ko thấy cthuc CoDA trong đó 
-https://repositorio.ufscar.br/bitstream/handle/ufscar/11034/v__final_ufscar.pdf?sequence=1&isAllowed=y || Penalized regression methods for compositional data || hmm... cấn cấn chỗ bị phạt nhỉ...nhưng có vẻ sẽ dùng khá tốt đó, có điều mọi thứ cũng có vẻ vắn tắt, cthuc nhiều và khó hiểu
-https://www.semanticscholar.org/paper/Multiple-linear-regression-modeling-for-data-Wang-Shangguan/c64167e81edb891fe1ab519e022a5ef42f9c1079 || nào rảnh tìm kiếm trong web này xem có tài liệu nào không
-https://d-nb.info/1221936638/34 cái này tuy ko có code nhưng có vẻ các hình vẽ biểu đồ khá đẹp
-
-tài liệu tiếng trung: 使用线性回归的源代码组合数据  || 使用 Python 进行成分数据分析
-https://blog.csdn.net/weixin_43329700/article/details/107811778 //chạy python
-https://zhuanlan.zhihu.com/p/561593013
-
-
 
 
 https://arxiv.org/search/?query=compositional+data&searchtype=all&source=header || trang web của đại học cornell 3
-tài liệu archison: hmmm...khá khó hiểu
-https://arxiv.org/pdf/2205.01158 7 ( trực tiếp đi vào các chủ đề đa hợp và tóm tắt khá ngắn gọn, phần sau khá khó hiểu, là nâng cấp của CoDA cho không gian nào đó)
-https://arxiv.org/pdf/2405.02551 8 ( nói tới tầm ảnh hưởng của CoDA với ngày nay, tập trung đề xuất một kiểm định trung bình hai mẫu)
-https://arxiv.org/pdf/2305.16842 8.5 (tóm tắt ngắn gọn lý thuyết, tập trung vào phân tích tài chính kế toán, có phần kiểm định giả thuyết nhỏ liên quan hồi quy)
-https://arxiv.org/pdf/2004.07881 8.5 ( tóm tắt lý thuyết, kết hợp việc giải thích ý nghĩa và sử dụng kết quả, kếp hợp hồi quy tuyến tính)
-https://arxiv.org/pdf/1508.01913 8 (tóm tắt lý thuyết, sử dụng phương pháp hồi quy mới để giảm việc giá trị null gây ảnh hưởng) 
-https://arxiv.org/pdf/2112.14865 9 (tóm tắt lý thuyết, nêu ra các đặc điểm CoDA khá ổn, tập trung hồi quy và PCA, cố gắng loại bỏ giá trị 0)
-https://arxiv.org/pdf/1904.05636 7.5 (Tập trung vào việc áp dụng các log của CoDA cho việc PCA) 
-https://arxiv.org/pdf/2002.05137 8.5 (tóm tắt lý thuyết, tập trung vào cải tiến cho hồi quy tuyến tính bằng CoDA)
-https://arxiv.org/pdf/2201.02451 8.5 (tóm tắt lý thuyết, có những nhận xét khá sâu về bài archison, giải thích cho bộ dữ liệu dân cư)
-https://arxiv.org/pdf/2110.07967 8 (tóm tắt lý thuyết, tập trung xây dựng, đánh giá và cải tiến CoDA cho không gian và hình học)
-https://arxiv.org/pdf/2012.10300 8 (tóm tắt lý thuyết, tập trung giải quyết các số 0 và mạng neron ANN với hồi quy)
-https://arxiv.org/pdf/2201.05197 9 (có vẻ một mô tả khá chi tiết, về hình thành phát triển CoDA của archison trong 40 năm)
-https://arxiv.org/pdf/2110.12439 7.5 (tập trung luôn giải quyết CoDA với dữ liệu ngân hàng châu âu)
-https://arxiv.org/pdf/2111.08953 8 (tóm tắt lý thuyết, bổ sung vào văn học về việc lựa chọn biến trong các bài toán phân tích hợp thành)
-https://arxiv.org/pdf/1512.05225 8 (tóm tắt lý thuyết,  đặc điểm của trung bình trung tâm một dữ liệu hợp thành với mô hình địa thống kê)
-https://arxiv.org/pdf/1704.06150 (using k-mean) 8.5 (tóm tắt lý thuyết, tập trung ứng dụng CoDA  với K-mean)
-https://arxiv.org/pdf/1412.5848 8 (Tập trung ứng dụng CoDA cho bóng chuyền và có thêm hồi quy tuyến tính, thiên dứng dụng hơn lý thuyết nhiều)
+
 
 
 https://www.jstor.org/ ||web cung cấp cái gì đó, rảnh thì vô ko thì thôi
 
-chuẩn bị tất cả nhứng thứ sẽ làm trong thứ 6, từ nhỏ nhất, dơn giản nhất, giải trí nhất tới trọng đại nhất buộc phải làm
-- liên hệ với linh hỏi thăm dự án data, có vẻ c ấy cũng chưa bắt đầu (c ấy định dấu rồi, có vẻ h chỉ muốn chill chill làm việc thôi)
-- mở lại các dự án về thương mại điện tử? dù cho nó đã quá thừa kẻ muốn kiếm tiền? (hmmm...có muốn làm nữa ko đây?)
-- chắc chắn phải hoàn thành yêu cầu của thầy về đồ án rồi, các video sẽ rất hữu dụng
- - nào nào, dự án web đã bỏ ngỏ quá lâu rồi, tại sao giờ vẫn chưa thấy làm?
-- ồ, chuẩn bị chủ đề tiếng anh gia đình để nói với zany nữa, hmmm
-- một góc thư thái để đọc sách? hmmm...cần dậy sớm mới có không khí và nhiệt độ tuyệt vời
-- rèn luyện khả năng tập trung...không được đa việc cùng lúc! đo mới là cách rèn tư duy tốt nhất!
-rồi dell làm đc cái gì luôn...súc vật
 
 
 https://nuhado.co/
